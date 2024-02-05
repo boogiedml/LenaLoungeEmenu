f16ea1a530cdb342a805a19c7ac92c374f412a6a
BOOGIEDML <isholasherifdeen@gmail.com>
2024-02-05 10:13:29 +0100

    ...

--- server/app.js
+++ server/app.js
@@ -17,13 +17,13 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 // app.use(verifyJwt);
-app.use(morgan("tiny"));
+app.use(morgan("tiny")); 
 
 // Routes
 app.get("/", (req, res) => {
   res.status(200).json({
     success: {
-      message: "Welcome to Viva Mexico API",
+      message: "Welcome to Lena Lounge API",
       status_code: 200,
     },
   });
